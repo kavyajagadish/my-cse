de5b258dc2a97be1f331f7d1407344783922df02
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-13 12:53:42 +0530

    Create int float.c

---
+++ int float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int myInt;
+    float myFloat;
+    printf("Enter an integer:");
+    scanf("%d",&myInt);
+    printf("Enter an float:");
+    scanf("%f",&myFloat);
+    printf("You entered the integer:%d\n",myInt);
+    printf("You entered the float:%d\n.2f\n",myFloat);
+    return 0;
+}
