5cc6ed492bff60c7736964e1f7ba9e169b219bed
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-13 11:07:59 +0530

    Create student age.c

---
+++ student age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    if("age>=19");
+    printf("enter the age");
+    scanf("%d",&age);
+    printf("student is adult");
+    if else("age<=19")
+    printf("enter the age");
+    scanf("%d",&age);
+    printf("student is not adult");
+    return 0;
+}
