0bb0ed618ca3d5209701dbc0239756a05985532d
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-13 11:54:16 +0530

    Create name,age,location.c

---
+++ name,age,location.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char name[]="sanju";
+    int age=19;
+    char location[]="tumukur";
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Location; %s\n", location);
+    return 0;
+}
