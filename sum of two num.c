c8b91b4d26c7fb998f8dbd2b549428ce9b699a3a
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-13 12:41:48 +0530

    Create sum of two num.c

---
+++ sum of two num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1, num2, sum;
+    printf("enter the first integer:");
+    scanf("%d",&num1);
+    printf("enter the second integer:");
+    scanf("%d",&num2);
+    sum = num1, num2;
+    printf("the sum of %d and %dis %d\n",num1,num2,sum);
+    return 0;
+}
