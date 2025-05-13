d8337f13d2abbd1e7c437ccc18ac5013ab5b021a
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-13 15:45:56 +0530

    Create name ,dateof birth,Mnum.c

---
+++ name ,dateof birth,Mnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int shridevi()
+{
+printf("kavyashree");
+printf("dateof birth");
+printf("age");
+printf("mobile number");
+}
+int main()
+{
+    shridevi(5,10);
+    return 0;
+}
