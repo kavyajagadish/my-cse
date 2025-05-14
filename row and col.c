de656370ea8022a53d99f4ffdcd86f235c745c2e
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-14 12:32:23 +0530

    Create row and col.c

---
+++ row and col.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int row,col;
+int sun[5][6]={
+    {1,2,3},{4,5,6}};
+int main(){
+    for(row=0;row=2;row++){
+        printf("%d",sun[row],[col]);
+    {
+    return 0;
+    }
+}
