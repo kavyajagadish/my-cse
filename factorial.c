26776afac124bde368cb6aeebe21640ba40121a7
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-14 15:43:32 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,c=1;
+    printf("enter the elements n\n");
+    scanf("%d",&n);
+    if (n==0){
+    printf("1");
+    }
+    else{
+        for(i=1;i<=n;i++){
+         c *=i;   
+        }
+        printf("%d",c);
+    }
+}
