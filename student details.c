e279f0a13fb2d4e678fdec9345fcac46a79a1573
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-14 10:53:33 +0530

    Create student details.c

---
+++ student details.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char name="sanju";
+    char collagename="SIET";
+    int age=19;
+    int usn="1SV25CS056";
+    char location="tumukur";
+    printf("Name: %s\n", name);
+    printf("Collagename: %s\n",collagename);
+    printf("Age: %d\n", age);
+    printf("USN: %d\n",usn);
+    printf("Location; %s\n", location);
+    return 0;
+}
