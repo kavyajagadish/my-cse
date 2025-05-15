8f4af2deacc460cab579d5720a8e705676e36efc
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-15 12:08:48 +0530

    Create size.c

---
+++ size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void even(int arr[],int size){
+    for (int i=0;i<size;i++){
+        if(arr[i]%2==0){
+            printf("6%d")
+        }
+    }
+}
+int maim
+    return 0;
+}
