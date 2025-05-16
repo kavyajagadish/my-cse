b3161b7f65fa38bfeeeeca704ddd2f13d4eef9a9
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-16 15:21:19 +0530

    Create malloc,realloc,free.c

---
+++ malloc,realloc,free.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+    int *arr=(int*)malloc(3*sizeof(int));
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr=(int *)realloc(arr,5*sizeof(int));
+    arr[3] = 40;
+    arr[4] = 50;
+    for(int i=0;i<5;i++)
+    printf("%d",arr[i]);
+    free(arr);
+    return 0;
+}
