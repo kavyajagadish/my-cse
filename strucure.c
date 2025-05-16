55e05b30c81d2710a83416ae2b944abc0e8d4151
kavyajagadish <jagadeeshsj1971@gmail.com>
2025-05-16 15:33:05 +0530

    Create strucure.c

---
+++ strucure.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    struct kavyajagadish {
+        char name[5];
+        int age;
+        float weight;
+    };
+    struct kavyajagadish k1;
+    printf("enter the name");
+    scanf("%s",&k1.name);
+    printf("enter the age");
+    scanf("%d",&k1.age);
+    printf("enter the weight");
+    scanf("%f",&k1.weight);
+    }
+   
